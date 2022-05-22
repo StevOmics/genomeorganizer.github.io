648b673c9cd47ef8a5af91e4557bdd32586e1e2d
Steve Ayers <californiastephen@outlook.com>
2022-05-21 19:29:19 -0700

    updated

--- docs/js/load_components.js
+++ docs/js/load_components.js
@@ -1,5 +1,7 @@
 $(function(){
     // console.log("Loading page components");
     $("#load-side").load("components/side.html");
-    $("#load-footer").load("components/footer.html");
+    // $("#load-ftr").load("components/ftr.html");
   });
+
+  // <!-- <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div> -->
